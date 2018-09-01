ebd9eab7b81f3477f5b6d8ffe4e1ca79676368c2
Joe Schmoe <michal.sul@wp.pl>
2018-09-02 01:10:11 +0200

    cpp. Is this the right directory?

---
+++ tools/scripting/ss13script/queue.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+template <typename T>
+class Queue
+{
+public:
+	T pop()
+	{
+		std::unique_lock<std::mutex> mlock(mutex_);
+		while (queue_.empty())
+		{
+			cond_.wait(mlock);
+		}
+		auto item = queue_.front();
+		queue_.pop();
+		return item;
+	}
+
+	void pop(T& item)
+	{
+		std::unique_lock<std::mutex> mlock(mutex_);
+		while (queue_.empty())
+		{
+			cond_.wait(mlock);
+		}
+		item = queue_.front();
+		queue_.pop();
+	}
+
+	void push(const T& item)
+	{
+		std::unique_lock<std::mutex> mlock(mutex_);
+		queue_.push(item);
+		mlock.unlock();
+		cond_.notify_one();
+	}
+
+	void push(T&& item)
+	{
+		std::unique_lock<std::mutex> mlock(mutex_);
+		queue_.push(std::move(item));
+		mlock.unlock();
+		cond_.notify_one();
+	}
+
+	std::queue<T> queue_;
+	std::mutex mutex_;
+	std::condition_variable cond_;
+};
