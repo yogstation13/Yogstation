AzriAnDr/HowlingVoid

Assimilation 2.0: Not quite a big enough PR to be worthy of the 2.0 title but almost (#42607)
* one

* ???

* big

* sprites

* it is done

* oofs

* no more bad jokes

* location -> reception

* i forgot a feature lmao

* yo lemme nerf this op power

* adjust removal and makes panic less insane

* hey i forgot another feature

* one mind removals

* *laser eyes meme*

* Disables multiple one minds for real this time

code/datums/hud.dm
@@ -28,8 +28,8 @@ GLOBAL_LIST_INIT(huds, list(
 	ANTAG_HUD_CLOCKWORK = new/datum/atom_hud/antag(),
 	ANTAG_HUD_BROTHER = new/datum/atom_hud/antag/hidden(),
 	ANTAG_HUD_HIVE = new/datum/atom_hud/antag/hidden(),
-	ANTAG_HUD_OBSESSED = new/datum/atom_hud/antag/hidden()
-	))
+	ANTAG_HUD_OBSESSED = new/datum/atom_hud/antag/hidden(),
+	ANTAG_HUD_HIVEAWAKE = new/datum/atom_hud/antag()	))
 
 /datum/atom_hud
 	var/list/atom/hudatoms = list() //list of all atoms which display this hud

yogstation13/Yogstation

code/datums/hud.dm
@@ -29,8 +29,8 @@ GLOBAL_LIST_INIT(huds, list(
 	ANTAG_HUD_BROTHER = new/datum/atom_hud/antag/hidden(),
 	ANTAG_HUD_VAMPIRE = new/datum/atom_hud/antag/hidden(), // Yogs
 	ANTAG_HUD_HIVE = new/datum/atom_hud/antag/hidden(),
-	ANTAG_HUD_CREEP = new/datum/atom_hud/antag/hidden()
-	))
+	ANTAG_HUD_CREEP = new/datum/atom_hud/antag/hidden(),
+	ANTAG_HUD_HIVEAWAKE = new/datum/atom_hud/antag()	))
 
 /datum/atom_hud
 	var/list/atom/hudatoms = list() //list of all atoms which display this hud