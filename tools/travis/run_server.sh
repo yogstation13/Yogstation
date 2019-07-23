cdee9e353be6589d465bb4d510e3569e67ac2fdf
alexkar598 <>
2019-07-23 14:35:05 -0400

    fixes the thing

--- tools/travis/run_server.sh
+++ tools/travis/run_server.sh
@@ -9,6 +9,6 @@ cp tools/travis/travis_config.txt travis_test/config/config.txt
 
 cd travis_test
 ln -s $HOME/libmariadb/libmariadb.so libmariadb.so
-DreamDaemon tgstation.dmb -close -trusted -verbose -params "test-run&log-directory=travis"
+DreamDaemon yogstation.dmb -close -trusted -verbose -params "test-run&log-directory=travis"
 cd ..
 cat travis_test/data/logs/travis/clean_run.lk
