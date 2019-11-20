a61fcd32e2c822f82317cc04f49f96b242e2c05c
Nichlas Pihl <nichlas00100@gmail.com>
2019-11-20 12:42:16 +0000

    Update tgstation_schema_prefixed.sql

--- SQL/tgstation_schema_prefixed.sql
+++ SQL/tgstation_schema_prefixed.sql
@@ -521,7 +521,7 @@ DROP TABLE IF EXISTS `SS13_achievements`;
 CREATE TABLE `SS13_achievements` (
 	`name` VARCHAR(32) NOT NULL,
 	`id` INT UNSIGNED NOT NULL,
-	`desc` VARCHAR(2048) NOT NULL,
+	`descr` VARCHAR(2048) NOT NULL,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
