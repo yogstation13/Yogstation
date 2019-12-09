b2455c1fd5130a18be632328175599b17779713f
Nichlas Pihl <nichlas0010@gmail.com>
2019-12-09 17:30:09 +0000

    Various tgui fixes (Chem Master/Dispenser, Air Alarm), small cleanup. (#48095)
    * Fix icons and button layout in Chem Dispenser, minor cleanup
    
    * Do not round volume in ChemMaster
    
    * Stricter ESLint rules
    
    * Rebuild tgui
    
    * No jumpy air alarm layout because of minuscule presence of random gases.
    
    * Holy shit linter did not detect a missing key

--- tgui-next/packages/tgui/routes.js
+++ tgui-next/packages/tgui/routes.js
@@ -1,4 +1,4 @@
-import {Achievements} from './interfaces/Achievements';
+import { Achievements } from './interfaces/Achievements';
 import { AiAirlock } from './interfaces/AiAirlock';
 import { AirAlarm } from './interfaces/AirAlarm';
 import { AirlockElectronics } from './interfaces/AirlockElectronics';
