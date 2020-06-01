9028d0168b19f9becc532b01dd8ea70c201e082c
TheGamerdk <carlgustav03@gmail.com>
2020-06-01 19:54:34 +0200

    Compile

--- tgui/packages/tgui/interfaces/CrewConsole.js
+++ tgui/packages/tgui/interfaces/CrewConsole.js
@@ -50,7 +50,7 @@ const HealthStat = props => {
   return (
     <Box
       inline
-      width={2}
+      width={4}
       color={COLORS.damageType[type]}
       textAlign="center">
       {value}
@@ -58,85 +58,104 @@ const HealthStat = props => {
   );
 };
 
-export const CrewConsole = () => {
-  return (
-    <Window resizable>
-      <Window.Content scrollable>
-        <Section minHeight="540px">
-          <CrewTable />
-        </Section>
-      </Window.Content>
-    </Window>
-  );
-};
-
-const CrewTable = (props, context) => {
-  const { act, data } = useBackend(context);
+export const CrewConsole = props => {
+  const { act, data } = useBackend(props);
   const sensors = data.sensors || [];
   return (
-    <Table>
-      <Table.Row>
-        <Table.Cell bold>
-          Name
-        </Table.Cell>
-        <Table.Cell bold collapsing />
-        <Table.Cell bold collapsing textAlign="center">
-          Vitals
-        </Table.Cell>
-        <Table.Cell bold>
-          Position
-        </Table.Cell>
-        {!!data.link_allowed && (
-          <Table.Cell bold collapsing>
-            Tracking
-          </Table.Cell>
-        )}
-      </Table.Row>
-      {sensors.map(sensor => (
-        <Table.Row key={sensor.name}>
-          <Table.Cell
-            bold={jobIsHead(sensor.ijob)}
-            color={jobToColor(sensor.ijob)}>
-            {sensor.name} ({sensor.assignment})
-          </Table.Cell>
-          <Table.Cell collapsing textAlign="center">
-            <ColorBox
-              color={healthToColor(
-                sensor.oxydam,
-                sensor.toxdam,
-                sensor.burndam,
-                sensor.brutedam)} />
-          </Table.Cell>
-          <Table.Cell collapsing textAlign="center">
-            {sensor.oxydam !== null ? (
-              <Box inline>
-                <HealthStat type="oxy" value={sensor.oxydam} />
-                {'/'}
-                <HealthStat type="toxin" value={sensor.toxdam} />
-                {'/'}
-                <HealthStat type="burn" value={sensor.burndam} />
-                {'/'}
-                <HealthStat type="brute" value={sensor.brutedam} />
-              </Box>
-            ) : (
-              sensor.life_status ? 'Alive' : 'Dead'
-            )}
-          </Table.Cell>
-          <Table.Cell>
-            {sensor.pos_x !== null ? sensor.area : 'N/A'}
-          </Table.Cell>
-          {!!data.link_allowed && (
-            <Table.Cell collapsing>
-              <Button
-                content="Track"
-                disabled={!sensor.can_track}
-                onClick={() => act('select_person', {
-                  name: sensor.name,
-                })} />
-            </Table.Cell>
-          )}
-        </Table.Row>
-      ))}
-    </Table>
+    <Window resizeable>
+      <Window.Content scrollable>
+        <Flex>
+          <Flex.Item>
+            <Section>
+              {data.z === 2 && (
+                <div className="map">
+                  {data["sensors"].map(sensor => (
+                    sensor.pos_x && (
+                      <div className="blip" style={
+                        `left:${
+                          (sensor.pos_x-data.minx)*(600/(data.maxx-data.minx))}px;
+                    top:${
+                      (data.maxy-sensor.pos_y)*(600/(data.maxx-data.minx))}px`
+                      } />
+                    )
+                  ))}
+                  <img src="minimap-1.png" width="600px"
+                    style={`-ms-interpolation-mode: nearest-neighbor`} />
+                </div>
+              )}
+            </Section>
+          </Flex.Item>
+          <Flex.Item>
+            <Section minHeight={90}>
+              <Table>
+                <Table.Row>
+                  <Table.Cell bold>
+                    Name
+                  </Table.Cell>
+                  <Table.Cell bold collapsing />
+                  <Table.Cell bold collapsing textAlign="center">
+                    Vitals
+                  </Table.Cell>
+                  <Table.Cell bold>
+                    Position
+                  </Table.Cell>
+                  {!!data.link_allowed && (
+                    <Table.Cell bold collapsing>
+                      Tracking
+                    </Table.Cell>
+                  )}
+                </Table.Row>
+                {sensors.map(sensor => (
+                  <Table.Row key={sensor.name}>
+                    <Table.Cell
+                      bold={jobIsHead(sensor.ijob)}
+                      color={jobToColor(sensor.ijob)}>
+                      {sensor.name} ({sensor.assignment})
+                    </Table.Cell>
+                    <Table.Cell collapsing textAlign="center">
+                      <ColorBox
+                        color={healthToColor(
+                          sensor.oxydam,
+                          sensor.toxdam,
+                          sensor.brutedam,
+                          sensor.brutedam)} />
+                    </Table.Cell>
+                    <Table.Cell collapsing textAlign="center">
+                      {sensor.oxydam !== null ? (
+                        <Box inline>
+                          <HealthStat type="oxy" value={sensor.oxydam} />
+                          {'/'}
+                          <HealthStat type="toxin" value={sensor.toxdam} />
+                          {'/'}
+                          <HealthStat type="burn" value={sensor.burndam} />
+                          {'/'}
+                          <HealthStat type="brute" value={sensor.brutedam} />
+                        </Box>
+                      ) : (
+                        sensor.life_status ? 'Alive' : 'Dead'
+                      )}
+                    </Table.Cell>
+                    <Table.Cell>
+                      {sensor.pos_x !== null ? sensor.area : 'N/A'}
+                    </Table.Cell>
+                    {!!data.link_allowed && (
+                      <Table.Cell collapsing>
+                        <Button
+                          content="Track"
+                          disabled={!sensor.can_track}
+                          onClick={() => act('select_person', {
+                            name: sensor.name,
+                          })} />
+                      </Table.Cell>
+                    )}
+                  </Table.Row>
+                ))}
+              </Table>
+            </Section>
+          </Flex.Item>
+        </Flex>
+      </Window.Content>
+    </Window>
+
   );
 };
