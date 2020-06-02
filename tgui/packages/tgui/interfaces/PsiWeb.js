57bf8ea824daf4ad23f2f394dca75d54c94224c2
TheGamerdk <carlgustav03@gmail.com>
2020-06-02 18:24:32 +0200

    uwu

---
+++ tgui/packages/tgui/interfaces/PsiWeb.js
@@ -0,0 +1,32 @@
+import { useBackend } from '../backend';
+import { Button, LabeledList, Section, Box } from '../components';
+import { Window } from '../layouts';
+
+export const PsiWeb = (props, context) => {
+  const { act, data } = useBackend(context);
+  return (
+    <Window>
+      <Window.Content>
+      <Section title="Psi Web">
+        <LabeledList>
+            <LabeledListItem label="Lucidity" right>
+                {data.lucidity}
+            </LabeledListItem>
+        </LabeledList>
+      </Section>
+      <Section title="Abilities">
+        {data.abilities.map(ability => (
+          <Fragment>
+            <Box>{ability.desc}</Box>
+            <Box>Psi use cost: {ability.psi_cost}</Box>
+            <Box>Cost to unlock: {ability.lucidity_cost}</Box>
+            <Button
+
+            />
+          </Fragment>
+        )}
+      </Section>
+      </Window.Content>
+    </Window>
+  );
+};
