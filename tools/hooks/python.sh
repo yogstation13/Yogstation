528e1517034c343bc98866cd0ae10d2cb9e86b5b
TheGamerdk <carlgustav03@gmail.com>
2020-06-03 15:38:50 +0200

    Update tools/hooks/python.sh
    Co-authored-by: Ling <lingbleed@gmail.com>

--- tools/hooks/python.sh
+++ tools/hooks/python.sh
@@ -13,7 +13,7 @@ fi
 PATHSEP=$($PY - <<'EOF'
 import sys, os
 if sys.version_info.major != 3 or sys.version_info.minor < 6:
-	sys.stderr.write("Python 3.6 or later is required, but you have:\n" + sys.version + "\n")
+	sys.stderr.write("Python 3.6 or later is required, but you have: " + sys.version + "\n")
 	exit(1)
 print(os.pathsep)
 EOF
