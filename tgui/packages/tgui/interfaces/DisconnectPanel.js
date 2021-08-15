e7db24b4fc35306e86751f1cc54bcf216360e373
Gabriel Adamson <adamson.g@gmail.com>
2021-08-15 16:57:16 -0500

    Hopefully linter happy

--- tgui/packages/tgui/interfaces/DisconnectPanel.js
+++ tgui/packages/tgui/interfaces/DisconnectPanel.js
@@ -22,48 +22,49 @@ export const DisconnectPanel = (props, context) => {
     return a.last.disconnect - b.last.disconnect;
   });
 
-  return (<Window
-    title="Disconnect Panel"
-    width={700}
-    height={700}
-    resizable>
-    <Window.Content scrollable>
-      <Section>
-        {data.users.map(user => (
-          <Collapsible
-            key={user.ckey}
-            color={user.connected ? 'green' : (user.last.living ? 'red' : 'yellow')}
-            title={user.ckey + " - "
-                  + (user.connected ? "Connected" : "Disconnected") + " - "
-                  + user.last.job + " - "
-                  + sec2time((data.world_time
-                          - (user.connected ? user.last.connect : user.last.disconnect))/10)}>
-            <Section>
-              <Button onClick={() => act('follow', { ckey: user.ckey })}>Follow</Button>
-              <Button onClick={() => act('player-panel', { ckey: user.ckey })}>Player Panel</Button>
-              <Button onClick={() => act('pm', { ckey: user.ckey })}>Private Message</Button>
-              <Button onClick={() => act('notes', { ckey: user.ckey })}>Notes</Button>
-              <Button
-                onClick={() => act('acryo', { ckey: user.ckey })}
-                disabled={user.connected}>
-                Admin Cryo
-              </Button>
-              <Collapsible title="History">
-                {user.history.reverse().map(datapoint => (
-                  <Section
-                    key={user.ckey}
-                    color={datapoint.living ? 'yellow' : 'green'}>
-                    {sec2time(datapoint.disconnect)}-{sec2time(datapoint.connect)}
-                    ({sec2time(datapoint.connect - datapoint.disconnect)})
-                    As {datapoint.job} ({datapoint.type})
-                  </Section>
-                ))}
-              </Collapsible>
-            </Section>
-          </Collapsible>
-        ))}
-      </Section>
-    </Window.Content>
-  </Window>
+  return (
+    <Window
+      title="Disconnect Panel"
+      width={700}
+      height={700}
+      resizable>
+      <Window.Content scrollable>
+        <Section>
+          {data.users.map(user => (
+            <Collapsible
+              key={user.ckey}
+              color={user.connected ? 'green' : (user.last.living ? 'red' : 'yellow')}
+              title={user.ckey + " - "
+                    + (user.connected ? "Connected" : "Disconnected") + " - "
+                    + user.last.job + " - "
+                    + sec2time((data.world_time
+                            - (user.connected ? user.last.connect : user.last.disconnect))/10)}>
+              <Section>
+                <Button onClick={() => act('follow', { ckey: user.ckey })}>Follow</Button>
+                <Button onClick={() => act('player-panel', { ckey: user.ckey })}>Player Panel</Button>
+                <Button onClick={() => act('pm', { ckey: user.ckey })}>Private Message</Button>
+                <Button onClick={() => act('notes', { ckey: user.ckey })}>Notes</Button>
+                <Button
+                  onClick={() => act('acryo', { ckey: user.ckey })}
+                  disabled={user.connected}>
+                  Admin Cryo
+                </Button>
+                <Collapsible title="History">
+                  {user.history.reverse().map(datapoint => (
+                    <Section
+                      key={user.ckey}
+                      color={datapoint.living ? 'yellow' : 'green'}>
+                      {sec2time(datapoint.disconnect)}-{sec2time(datapoint.connect)}
+                      ({sec2time(datapoint.connect - datapoint.disconnect)})
+                      As {datapoint.job} ({datapoint.type})
+                    </Section>
+                  ))}
+                </Collapsible>
+              </Section>
+            </Collapsible>
+          ))}
+        </Section>
+      </Window.Content>
+    </Window>
   );
 };
