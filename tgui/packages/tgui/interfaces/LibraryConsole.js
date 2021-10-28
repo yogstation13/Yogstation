533b985119c1eeb54e1ceed819d2f473aa2f516a
Redmoogle <dakotamew@gmail.com>
2021-10-28 18:05:01 -0400

    Hell

--- tgui/packages/tgui/interfaces/LibraryConsole.js
+++ tgui/packages/tgui/interfaces/LibraryConsole.js
@@ -144,196 +144,193 @@ export const BookDB = (props, context) => {
 export const LibraryMangement = (props, context) => {
   const { act, data } = useBackend(context);
   const [tab2, setTab2] = useSharedState(context, 'tab2', 1);
-  var [checkoutTime, setTime] = useSharedState(context, "checkoutTime", 5)
-  var [category, setcategory] = useSharedState(context, "category", "Fiction")
-  const validcategorys=["Fiction", "Non-Fiction", "Adult", "Reference", "Religion"]
+  let [checkoutTime, setTime] = useSharedState(context, "checkoutTime", 5);
+  let [category, setcategory] = useSharedState(context, "category", "Fiction");
+  const validcategorys=["Fiction", "Non-Fiction", "Adult", "Reference", "Religion"];
   const {
     emagged,
     scanner,
     checkouts,
   } = data;
   return (
-  <Box>
     <Box>
-      <Divider />
-      <Tabs fontSize="11px" py={1.5} mx={1}>
-        <Tabs.Tab
-          lineHeight="23px"
-          onClick={() => setTab2(1)}
-          selected={tab2 === 1}
-          >
-          General Inventory
-        </Tabs.Tab>
-        <Tabs.Tab
-          lineHeight="23px"
-          onClick={() => setTab2(2)}
-          selected={tab2 === 2}
-          >
-          Checked Out
-        </Tabs.Tab>
-        <Tabs.Tab
-          lineHeight="23px"
-          onClick={() => setTab2(3)}
-          selected={tab2 === 3}
-          >
-          Book Check Out
-        </Tabs.Tab>
-        <Tabs.Tab
-          lineHeight="23px"
-          onClick={() => setTab2(4)}
-          selected={tab2 === 4}
-          >
-          External Archive
-        </Tabs.Tab>
-        <Tabs.Tab
-          lineHeight="23px"
-          onClick={() => setTab2(5)}
-          selected={tab2 === 5}
-          >
-          Upload to Archive
-        </Tabs.Tab>
-        <Tabs.Tab
-          lineHeight="23px"
-          onClick={() => setTab2(6)}
-          selected={tab2 === 6}
-          >
-          Upload to Newscaster
-        </Tabs.Tab>
-        <Tabs.Tab
-          lineHeight="23px"
-          onClick={() => setTab2(7)}
-          selected={tab2 === 7}
-          >
-          Print Corporate Materials
-        </Tabs.Tab>
-        {!!emagged && (
+      <Box>
+        <Divider />
+        <Tabs fontSize="11px" py={1.5} mx={1}>
           <Tabs.Tab
             lineHeight="23px"
-            onClick={() => setTab2(8)}
-            selected={tab2 === 8}
-            >
-            Access the Forbidden Lore Vault
+            onClick={() => setTab2(1)}
+            selected={tab2 === 1}>
+            General Inventory
           </Tabs.Tab>
-        )}
+          <Tabs.Tab
+            lineHeight="23px"
+            onClick={() => setTab2(2)}
+            selected={tab2 === 2}>
+            Checked Out
+          </Tabs.Tab>
+          <Tabs.Tab
+            lineHeight="23px"
+            onClick={() => setTab2(3)}
+            selected={tab2 === 3}>
+            Book Check Out
+          </Tabs.Tab>
+          <Tabs.Tab
+            lineHeight="23px"
+            onClick={() => setTab2(4)}
+            selected={tab2 === 4}>
+            External Archive
+          </Tabs.Tab>
+          <Tabs.Tab
+            lineHeight="23px"
+            onClick={() => setTab2(5)}
+            selected={tab2 === 5}>
+            Upload to Archive
+          </Tabs.Tab>
+          <Tabs.Tab
+            lineHeight="23px"
+            onClick={() => setTab2(6)}
+            selected={tab2 === 6}>
+            Upload to Newscaster
+          </Tabs.Tab>
+          <Tabs.Tab
+            lineHeight="23px"
+            onClick={() => setTab2(7)}
+            selected={tab2 === 7}>
+            Print Corporate Materials
+          </Tabs.Tab>
+          {!!emagged && (
+            <Tabs.Tab
+              lineHeight="23px"
+              onClick={() => setTab2(8)}
+              selected={tab2 === 8}>
+              Access the Forbidden Lore Vault
+            </Tabs.Tab>
+          )}
         </Tabs>
         <Divider />
       </Box>
       <Section>
-      {tab2 === 1 && (
-        <Section>
-          <h3>Inventory</h3><br />
-          ERR 404: Inventory Not Found
-        </Section>
-      )}
-      {tab2 === 2 && (
-        <Section>
-          <h3>Checked Out Books</h3>
-          {Object.keys(checkouts).map((user) => (
-            <Section>
-              {Object.keys(checkouts[user]["books"]).map((book) => (
-                <Box>
-                  {!checkouts[user]["books"][book]["overdue"] ?
-                  <Box>
-                    {checkouts[user]["books"][book]["title"]} by {checkouts[user]["books"][book]["author"]}<br />
-                    <Box style="padding-left: 10px;">
-                      Borrowed By: {checkouts[user]["books"][book]["borrower"]}<br />
-                      Due in {checkouts[user]["books"][book]["due"]} Minutes
-                      </Box>
-                  </Box>
-                  :
-                  <Box>
-                    <Section>
-                      {checkouts[book]["books"]["title"]} by {checkouts[book]["books"]["author"]}
-                        {checkouts[book]["books"]["borrower"]}: Overdue!!!
-                    </Section>
-                  </Box>
-                  }
-                </Box>
-              ))}
-              <Divider />
-            </Section>
-          ))}
-        </Section>
-      )}
-      {tab2 === 3 && (
-        <Section>
-          <h3>Check Out a Book</h3>
-          {!!scanner ?
+        {tab2 === 1 && (
           <Section>
-            Book: {scanner["title"]} by {scanner["author"]}<br /><br />
-            Checkout Time <br />
-            <NumberInput
-              minValue = {5}
-              maxValue = {60}
-              value = {checkoutTime}
-              unit = {"Minutes"}
-              onChange={(e, value) => setTime(value)}
+            <h3>Inventory</h3><br />
+            ERR 404: Inventory Not Found
+          </Section>
+        )}
+        {tab2 === 2 && (
+          <Section>
+            <h3>Checked Out Books</h3>
+            {Object.keys(checkouts).map(user => (
+              <Section key={user}>
+                {Object.keys(checkouts[user]["books"]).map(book => (
+                  <Box key={book}>
+                    {!checkouts[user]["books"][book]["overdue"]
+                      ?(
+                        <Box>
+                          {checkouts[user]["books"][book]["title"]} by {checkouts[user]["books"][book]["author"]}<br />
+                          <Box pl={0.5}>
+                            Borrowed By: {checkouts[user]["books"][book]["borrower"]}<br />
+                            Due in {checkouts[user]["books"][book]["due"]} Minutes
+                          </Box>
+                        </Box>
+                      ):(
+                        <Box>
+                          <Section>
+                            {checkouts[book]["books"]["title"]} by {checkouts[book]["books"]["author"]}
+                            {checkouts[book]["books"]["borrower"]}: Overdue!!!
+                          </Section>
+                        </Box>
+                      )}
+                  </Box>
+                ))}
+                <Divider />
+              </Section>
+            ))}
+          </Section>
+        )}
+        {tab2 === 3 && (
+          <Section>
+            <h3>Check Out a Book</h3>
+            {scanner
+              ?(
+                <Section>
+                  Book: {scanner["title"]} by {scanner["author"]}<br /><br />
+                  Checkout Time <br />
+                  <NumberInput
+                    minValue={5}
+                    maxValue={60}
+                    value={checkoutTime}
+                    unit={"Minutes"}
+                    onChange={(e, value) => setTime(value)}
+                  />
+                  <Button
+                    content="Check Out"
+                    onClick={() => act('checkoutbook', {
+                      id: scanner["id"],
+                      time: checkoutTime,
+                      title: scanner["title"],
+                      author: scanner["author"],
+                    })} />
+                </Section>
+              ):(
+                <Section>
+                  No book loaded in the scanner
+                </Section>
+              )}
+          </Section>
+        )}
+        {tab2 === 4 && (
+          <Box>
+            <BookDB />
+          </Box>
+        )}
+        {tab2 === 5 && (
+          <Box>
+            <h3>Upload a New Title</h3>
+            {scanner
+              ?(
+                <Section>
+                  {scanner.title} by {scanner.author}
+                  <Dropdown
+                    options={validcategorys}
+                    selected={category}
+                    onSelected={value => setcategory(value)}
+                  />
+                  <br />
+                  <Button
+                    content="Upload"
+                    onClick={() => act('uploadbook', {
+                      id: scanner["id"],
+                      category: category,
+                    })} />
+                </Section>
+              ):(
+                <Box>
+                  Scanner not found
+                </Box>
+              )}
+          </Box>
+        )}
+        {tab2 === 6 && (
+          <Section>
+            <h3>Post Title to Newscaster</h3>
+          </Section>
+        )}
+        {tab2 === 7 && (
+          <Section>
+            <h3>Universal Printing Module</h3>
+            <Dropdown
+              options={["A", "B"]}
+              selected={"A"}
+              onSelected={value => act('printtype', {
+                type: value,
+              })}
             />
-            <Button
-                content="Check Out"
-                onClick={() => act('checkoutbook', {
-                  id: scanner["id"],
-                  time: checkoutTime,
-                  title: scanner["title"],
-                  author: scanner["author"]
-            })} />
-          </Section> :
-          <Section>No book loaded in the scanner</Section>
-          }
-        </Section>
-      )}
-      {tab2 === 4 && (
-        <Box>
-          <BookDB />
-        </Box>
-      )}
-      {tab2 === 5 && (
-        <Box>
-          <h3>Upload a New Title</h3>
-          {!!scanner ?
-            <Section>
-              {scanner.title} by {scanner.author}
-              <Dropdown
-                options={validcategorys}
-                selected={category}
-                onSelected={value => setcategory(value)
-                }
-              />
-              <br />
-              <Button
-                content="Upload"
-                onClick={() => act('uploadbook', {
-                  id: scanner["id"],
-                  category: category
-            })} />
-            </Section>
-            :
-            <Box>
-              Scanner not found
-            </Box>
-        }
-        </Box>
-      )}
-      {tab2 === 6 && (
-        <Section>
-          <h3>Post Title to Newscaster</h3>
-        </Section>
-      )}
-      {tab2 === 7 && (
-        <Section>
-        <h3>Universal Printing Module</h3>
-        <Dropdown
-            options={["A", "B"]}
-            selected={"A"}
-            onSelected={value => act('printtype', {
-              type: value
-            })}
-          />
-        </Section>
-      )}
-      {tab2 === 8 && (
-        <h3></h3>
-      )}
-    </Section>
-  </Box>
-); };
+          </Section>
+        )}
+        {tab2 === 8 && (
+          <h3 />
+        )}
+      </Section>
+    </Box>
+  ); };
