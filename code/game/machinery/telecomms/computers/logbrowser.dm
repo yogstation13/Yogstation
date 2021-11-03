af61133e2ef9c7827716152b032f9931fe7f0f97
Gary Lafortune <mom-puter@hotmail.com>
2021-11-02 21:17:55 -0500

    Refactors the telecomm log computer's GUI
    Specifically this is the computer that handles showing the radio logs, not the PDA ones.

--- code/game/machinery/telecomms/computers/logbrowser.dm
+++ code/game/machinery/telecomms/computers/logbrowser.dm
@@ -1,22 +1,111 @@
-
+#define MONITOR_MAINMENU 0
+#define MONITOR_SERVERLOGS 1
 
 /obj/machinery/computer/telecomms/server
 	name = "telecommunications server monitoring console"
 	icon_screen = "comm_logs"
-	desc = "Has full access to all details and record of the telecommunications network it's monitoring."
+	desc = "A computer dedicated to monitoring telecommuncation server logs."
 
-	var/screen = 0				// the screen number:
-	var/list/servers = list()	// the servers located by the computer
+	var/screen_state = MONITOR_MAINMENU	 // the screen state
+	var/list/cached_server_list = list() // The servers located by the computer
 	var/obj/machinery/telecomms/server/SelectedServer
 
 	var/network = "NULL"		// the network to probe
-	var/temp = ""				// temporary feedback messages
 
-	var/universal_translate = 0 // set to 1 if it can translate nonhuman speech
+	var/universal_translate = FALSE // set to TRUE if it can translate nonhuman speech
 
 	req_access = list(ACCESS_TCOMSAT)
 	circuit = /obj/item/circuitboard/computer/comm_server
+	tgui_id = "LogBrowser"
 
+/obj/machinery/computer/telecomms/server/ui_interact(mob/user, datum/tgui/ui)
+	ui = SStgui.try_update_ui(user,src,ui)
+	if(!ui)
+		ui = new(user,src,"LogBrowser")
+		ui.open()
+
+/obj/machinery/computer/telecomms/server/ui_act(action, list/params)
+	if(..())
+		return
+	switch(action)
+		if("Back")
+			screen_state = MONITOR_MAINMENU
+			SelectedServer = null
+			return TRUE
+		if("ViewServer")
+			var/id = params["server_id"]
+			if(!id)
+				return
+			for(var/machine in cached_server_list)
+				var/obj/machinery/telecomms/server/m = machine
+				if(m.id == id)
+					SelectedServer = m
+					break
+			if(SelectedServer)
+				screen_state = MONITOR_SERVERLOGS
+				return TRUE
+			return
+		if("DeleteLog")
+			if(!SelectedServer)
+				return
+			var/name = params["name"]
+			if(!name || !istext(name) || length(name) > 1024)
+				return
+			for(var/l in SelectedServer.log_entries)
+				var/datum/comm_log_entry/log = l
+				if(log.name == name)
+					SelectedServer.log_entries.Remove(log)
+					return TRUE
+			return
+		if("SetNetwork")
+			var/net = params["network"]
+			if(!net || !istext(net) || isnotpretty(net) || length(net) > 15)
+				return
+			network = params["network"]
+			return TRUE
+		if("Scan")
+			if(cached_server_list.len > 0)
+				cached_server_list = list()
+			for(var/obj/machinery/telecomms/server/T in range(TELECOMMS_SCAN_RANGE, src))
+				if(T.network == network)
+					cached_server_list.Add(T)
+			return TRUE
+		if("Refresh")
+			return TRUE // Welp, you asked for it
+
+/obj/machinery/computer/telecomms/server/ui_data(mob/user)
+	var/list/data = list()
+	data["screen_state"] = screen_state
+	data["network"] = network
+	if(screen_state == MONITOR_MAINMENU)
+		var/list/servers = list()
+		for(var/machine in cached_server_list)
+			var/obj/machinery/telecomms/server/m = machine
+			servers.Add(m.id)
+		data["servers"] = servers
+	if(screen_state == MONITOR_SERVERLOGS)
+		data["selected_name"] = SelectedServer.id
+		data["totaltraffic"] = SelectedServer.log_entries.len
+		data["define_max_storage"] = SERVER_LOG_STORAGE_MAX
+		data["logs"] = list()
+		for(var/l in SelectedServer.log_entries)
+			var/datum/comm_log_entry/log = l
+			var/list/datalog = list()
+			datalog["is_corrupt"] = (log.input_type == "Corrupt File")
+			datalog["is_error"] = (log.input_type == "Execution Error")
+			datalog["name"] = log.parameters["name"] || "Unknown"
+			datalog["job"] = log.parameters["job"] || FALSE
+			datalog["message"] = log.parameters["message"] || "***"
+			datalog["packet_id"] = log.name // since this is some MD5 thing we can actually use this to ID this packet later in ui_act()
+			data["logs"] += list(datalog)
+
+	return data
+
+#undef MONITOR_MAINMENU
+#undef MONITOR_SERVERLOGS
+
+
+/*
 /obj/machinery/computer/telecomms/server/ui_interact(mob/user)
 	. = ..()
 	var/dat = "<HTML><HEAD><meta charset='UTF-8'><TITLE>Telecommunication Server Monitor</TITLE></HEAD><BODY><center><b>Telecommunications Server Monitor</b></center>"
@@ -213,3 +302,4 @@
 /obj/machinery/computer/telecomms/server/attackby()
 	. = ..()
 	updateUsrDialog()
+*/
---
+++ tgui/packages/tgui/interfaces/LogBrowser.js
@@ -0,0 +1,150 @@
+import { useBackend } from '../backend';
+import { Box, Button, Divider, Section, LabeledList, Icon, NoticeBox, Input, ProgressBar, Tooltip } from '../components';
+import { Window } from '../layouts';
+
+
+const generate_server_list = (servers,act) => {
+    if(!servers || servers.length === 0)
+    {
+        return (
+            <NoticeBox textAlign='center' warning>
+            <Icon name='exclamation-triangle' size={5}></Icon><br/>
+            No Servers stored in Buffer!<br/>
+            Please scan for servers to continue.
+            </NoticeBox>
+        );
+    }
+    const mapped_servers = servers.map((server) => {
+        return (<LabeledList.Item
+          label={server}
+          buttons={(
+            <Button onClick ={() => act('ViewServer', {'server_id': server})}>
+              Select Server
+            </Button>
+          )}>
+        </LabeledList.Item>);
+      });
+    return mapped_servers;
+}
+
+const generate_logs = (logs,act) => {
+    if(!logs || logs.length === 0)
+    {
+        return (
+            <NoticeBox textAlign='center' warning>
+            <Icon name='exclamation-triangle' size={5}></Icon><br/>
+            No logs detected!<br/>
+            </NoticeBox>
+        );
+    }
+    let mapped_logs = [];
+    for (const [index, log] of Object.entries(logs)) {
+        if(log["is_error"])
+        {
+            mapped_logs.push(
+            <NoticeBox textAlign='center' warning mb={5}>
+            <Icon name='exclamation-triangle' size={2}></Icon><br/>
+            {log["message"]}
+            </NoticeBox>);
+        }
+        else
+        {
+            if(log["job"])
+            {
+                mapped_logs.push(
+                    <Box backgroundColor="#333340" mb={5} nowrap={false}>
+                        <Box>
+                            Data Packet ({log["packet_id"]})
+                        </Box>
+                        <b>Name: </b> {log["name"]}<br/>
+                        <b>Job: </b> {log["job"]}<br/>
+                        <b>Received Message: </b> {log["message"]}<br/>
+                    </Box>
+                );
+                continue;
+            }
+            mapped_logs.push(
+            <Box backgroundColor="#333340" mb={5} nowrap={false}>
+                <Box>
+                    Data Packet ({log["packet_id"]})
+                </Box>
+                <b>Name: </b> {log["name"]}<br/>
+                <b>Received Message: </b> {log["message"]}<br/>
+
+            </Box>);
+        }
+    }
+    return mapped_logs;
+}
+
+export const LogBrowser = (props, context) => {
+    const { act, data } = useBackend(context);
+    const {
+    screen_state,
+    network,
+    //
+    servers,
+    //
+    selected_name,
+    logs,
+    totaltraffic,
+    define_max_storage,
+    } = data;
+
+    if(screen_state === 0) // MAIN MENU
+    {
+    return (
+        <Window 
+            title="Telecommunications Monitoring Console"
+            width={460}
+            height={550}>
+            <Window.Content>
+            <Box>
+                Current network: <Input placeholder={network ? network : "NO NETWORK"} onChange={ (e,value) => {act('SetNetwork',{"network": value})}}></Input>
+            </Box>
+            <Divider></Divider>
+            <Button onClick={(e,value) => {act('Scan',{})}}><Icon name='binoculars'></Icon>Scan for Servers</Button>
+            <Section>
+               {generate_server_list(servers,act)}
+            </Section>
+            </Window.Content>
+        </Window>
+        );
+    }
+    else if(screen_state === 1) // SERVER LOGS
+    {
+    return (
+        <Window 
+            title="Telecommunications Monitoring Console"
+            width={460}
+            height={550}>
+            <Window.Content scrollable>
+            <Button onClick={(e,value) => {act('Back',{})}}><Icon name='backward'></Icon>Back</Button>
+            <Divider></Divider>
+            <Section title="Server Information">
+            <Icon name='wifi' size={1}></Icon>&emsp;Network:<br/> {network}<br/><br/>
+            <Icon name='server' size={1}></Icon>&emsp;Server:<br/>{selected_name}<br/><br/>
+            <Box position="relative">
+            <Icon name='project-diagram'></Icon>&emsp;Total Traffic Storage:<Tooltip
+                position="bottom-right"
+                content="The total log storage capacity of the server. Old logs are deleted if the storage becomes full." />
+            <ProgressBar
+                ranges={{
+                    good: [0, define_max_storage * 0.50],
+                    average: [define_max_storage * 0.50, define_max_storage * 0.70],
+                    bad: [define_max_storage * 0.70, define_max_storage],
+                }}
+                minValue={0}
+                maxValue={define_max_storage}
+                value={totaltraffic}>{totaltraffic} GB</ProgressBar>
+            </Box>
+            </Section>
+            <Section title="Stored Logs">
+                {generate_logs(logs,act)}
+            </Section>
+            </Window.Content>
+        </Window>
+        );
+    }
+  
+};
