6b5cabccd99465ecc732e953425c8fbbff645d28
adamsong <adamsong@users.noreply.github.com>
2022-08-09 16:13:58 -0400

    Linter

--- tgui/packages/tgui/interfaces/PermissionsPanel.js
+++ tgui/packages/tgui/interfaces/PermissionsPanel.js
@@ -3,7 +3,7 @@ import { Box, Button, Collapsible, Flex, Section, Tabs } from "../components";
 import { Layout, Window } from "../layouts";
 import { createLogger } from "../logging";
 
-const logger = createLogger('PermissionsPanel')
+const logger = createLogger('PermissionsPanel');
 
 export const PermissionsPanel = (props, context) => {
   const { act, data } = useBackend(context);
@@ -19,70 +19,70 @@ export const PermissionsPanel = (props, context) => {
           color="green"
           mb={1}
           onClick={() => act('addNewAdmin')}>
-            Add New
-          </Button>
-      {data.admins.map((admin, i) => (
-        <AdminDisplay
-          key = {i}
-          data = {admin}
-          act = {act} />
-      ))}
+          Add New
+        </Button>
+        {data.admins.map((admin, i) => (
+          <AdminDisplay
+            key={i}
+            data={admin}
+            act={act} />
+        ))}
       </Window.Content>
     </Window>
   );
 };
 
 export const AdminDisplay = props => {
-  const { data, act } = props
+  const { data, act } = props;
   const {
     ckey,
     rank,
     protected_admin,
     protected_rank,
     rights,
-    deadminned
+    deadminned,
   } = data;
   return (
     <Section
       title={`${ckey} - ${rank}`}
       buttons={(
         <Button
-          icon='trash'
+          icon="trash"
           color="bad"
           onClick={() => act('removeAdmin', {
-            ckey: ckey
+            ckey: ckey,
           })} />
       )} >
-        <Button onClick={() => act('forceSwapAdmin', {
-          ckey: ckey
+      <Button onClick={() => act('forceSwapAdmin', {
+        ckey: ckey,
+      })}>
+        Force { deadminned ? "Re" : "De" }admin
+      </Button>
+      <Button onClick={() => act('resetMFA', {
+        ckey: ckey,
+      })}>
+        Reset MFA
+      </Button>
+      <Button
+        disabled={protected_admin}
+        onClick={() => act('editRank', {
+          ckey: ckey,
         })}>
-          Force { deadminned ? "Re" : "De" }admin
-        </Button>
-        <Button onClick={() => act('resetMFA', {
-          ckey: ckey
-        })}>
-          Reset MFA
-        </Button>
-        <Button
-          disabled={protected_admin}
-          onClick={() => act('editRank', {
-            ckey: ckey
-          })}>
-          Change Rank
-        </Button>
-        <Collapsible
-          title="Permissions"
-          buttons={(
-            <Button
-              icon="edit"
-              content="Edit"
-              disabled={protected_rank}
-              onClick={() => act('editPerms', {
-                ckey: ckey
-              })}/>
-          )}>
-          { rights }
-        </Collapsible>
+        Change Rank
+      </Button>
+      <Collapsible
+        title="Permissions"
+        buttons={(
+          <Button
+            icon="edit"
+            content="Edit"
+            disabled={protected_rank}
+            onClick={() => act('editPerms', {
+              ckey: ckey,
+            })} />
+        )}>
+        { rights }
+      </Collapsible>
     </Section>
   );
-}
+};
