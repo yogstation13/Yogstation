3274f41fdee825ae619075b31eba3760c8e41b87
Bop <smmsmsmsmmm@gmail.com>
2022-08-14 13:54:16 +0700

    whoopsss

--- tgui/packages/tgui/interfaces/CrewConsole.js
+++ tgui/packages/tgui/interfaces/CrewConsole.js
@@ -152,7 +152,7 @@ export const CrewConsoleContent = (props, context) => {
                 <Table.Cell collapsing textAlign="center">
                   {sensor.is_robot ? <Icon name="wrench" color="#B7410E" size={1} /> : ""}
                 </Table.Cell>
-                <Table.Cell collapsing textAlign="center">#c71402
+                <Table.Cell collapsing textAlign="center">
                   {sensor.oxydam !== null ? (
                     <Icon
                       name={healthToAttribute( // yogs -- show death when dead
