2afe344cbd3867854e779338d2c536bd0c77214a
TheGamerdk <5618080+TheGamerdk@users.noreply.github.com>
2022-08-30 17:19:03 +0200

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -6,7 +6,7 @@
 # byond version
 # Extracted from the Dockerfile. Change by editing Dockerfile's FROM command.
 export BYOND_MAJOR=514
-export BYOND_MINOR=1583
+export BYOND_MINOR=1585
 
 #rust_g git tag
 export RUST_G_VERSION=0.7.0
