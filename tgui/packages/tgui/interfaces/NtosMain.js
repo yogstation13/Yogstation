ba4504dbd40e43a9ee36f1b8d6119566c60b6095
Chubbygummibear <kullen.ds@gmail.com>
2022-09-12 23:10:37 -0700

    that should be it

--- tgui/packages/tgui/interfaces/NtosMain.js
+++ tgui/packages/tgui/interfaces/NtosMain.js
@@ -1,5 +1,5 @@
 import { useBackend } from '../backend';
-import { Button, ColorBox, Section, Table } from '../components';
+import { Button, ColorBox, Section, Table, Flex } from '../components';
 import { NtosWindow } from '../layouts';
 
 export const NtosMain = (props, context) => {
@@ -24,21 +24,42 @@ export const NtosMain = (props, context) => {
       height={500}
       resizable>
       <NtosWindow.Content scrollable>
-        {!!has_light && (
+        {(!!has_light || programs.some(program => program.name==='themeify')) && (
           <Section>
-            <Button
-              width="144px"
-              icon="lightbulb"
-              selected={light_on}
-              onClick={() => act('PC_toggle_light')}>
-              Flashlight: {light_on ? 'ON' : 'OFF'}
-            </Button>
-            <Button
-              ml={1}
-              onClick={() => act('PC_light_color')}>
-              Color:
-              <ColorBox ml={1} color={comp_light_color} />
-            </Button>
+            <Flex>
+              {!!has_light && (
+                <Flex.Item>
+                  <Button
+                    width="144px"
+                    icon="lightbulb"
+                    selected={light_on}
+                    onClick={() => act('PC_toggle_light')}>
+                    Flashlight: {light_on ? 'ON' : 'OFF'}
+                  </Button>
+                  <Button
+                    ml={1}
+                    onClick={() => act('PC_light_color')}>
+                    Color:
+                    <ColorBox ml={1} color={comp_light_color} />
+                  </Button>
+                </Flex.Item>
+              )}
+
+              <Flex.Item
+                ml={1}
+                grow={1}>
+                {programs.some(program => program.name==='themeify') && (
+                  <Button
+                    fluid
+                    align="center"
+                    onClick={() => act('PC_runprogram', {
+                      name: 'themeify',
+                    })}>
+                    Change Theme
+                  </Button>
+                )}
+              </Flex.Item>
+            </Flex>
           </Section>
         )}
         {!!cardholder && (
@@ -81,37 +102,52 @@ export const NtosMain = (props, context) => {
             </Table>
           </Section>
         )}
-        <Section title="Programs">
-          <Table>
-            {programs.map(program => (
-              <Table.Row key={program.name}>
-                <Table.Cell>
-                  <Button
-                    fluid
-                    color={program.alert ? 'yellow' : 'transparent'}
-                    icon={program.icon}
-                    content={program.desc}
-                    onClick={() => act('PC_runprogram', {
-                      name: program.name,
-                    })} />
-                </Table.Cell>
-                <Table.Cell collapsing width="18px">
-                  {!!program.running && (
-                    <Button
-                      color="transparent"
-                      icon="times"
-                      tooltip="Close program"
-                      tooltipPosition="left"
-                      onClick={() => act('PC_killprogram', {
-                        name: program.name,
-                      })} />
-                  )}
-                </Table.Cell>
-              </Table.Row>
-            ))}
-          </Table>
-        </Section>
+        <ProgramsTable />
       </NtosWindow.Content>
     </NtosWindow>
+  );
+};
+
+const ProgramsTable = (props, context) => {
+  const { act, data } = useBackend(context);
+  const {
+    programs = [],
+  } = data;
+  // add the program filename to this list to have it excluded from the main menu program list table
+  const programs_to_be_filtered = ["themeify"];
+  const filtered_programs
+  = programs.filter(program => !programs_to_be_filtered.includes(program.name));
+
+  return (
+    <Section title="Programs">
+      <Table>
+        {filtered_programs.map(program => (
+          <Table.Row key={program.name}>
+            <Table.Cell>
+              <Button
+                fluid
+                color={program.alert ? 'yellow' : 'transparent'}
+                icon={program.icon}
+                content={program.desc}
+                onClick={() => act('PC_runprogram', {
+                  name: program.name,
+                })} />
+            </Table.Cell>
+            <Table.Cell collapsing width="18px">
+              {!!program.running && (
+                <Button
+                  color="transparent"
+                  icon="times"
+                  tooltip="Close program"
+                  tooltipPosition="left"
+                  onClick={() => act('PC_killprogram', {
+                    name: program.name,
+                  })} />
+              )}
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table>
+    </Section>
   );
 };
