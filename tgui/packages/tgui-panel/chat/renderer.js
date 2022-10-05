9f9bab68ad0218490bbc5e941d776a6b82a4fac4
Redmoogle <dakotamew@gmail.com>
2022-10-05 07:46:19 -0400

    poke linter

--- tgui/packages/tgui-panel/chat/renderer.js
+++ tgui/packages/tgui-panel/chat/renderer.js
@@ -176,7 +176,7 @@ class ChatRenderer {
       this.highlightColor = null;
       return;
     }
-    const allowedRegex = /^[a-z0-9_-\s]+$/ig;
+    //const allowedRegex = /^[a-z0-9_-\s]+$/ig;
     const lines = String(text)
       .split(',')
       // eslint-disable-next-line no-useless-escape
