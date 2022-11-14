fadf9ff6da8787eeeb937ed3fcedbace4dbbe403
Chubbygummibear <kullen.ds@gmail.com>
2022-11-13 20:31:59 -0800

    pixel perfect

--- code/modules/vending/security_armaments.dm
+++ code/modules/vending/security_armaments.dm
@@ -6,7 +6,16 @@
 	icon_state = "armament" // BAIOMU REPLACE THIS WITH YOUR SPRITE
 	layer = 2.9
 	density = TRUE
+	var/list/inventory = list()
+	
+	contents = newlist(/obj/item/gun/energy/disabler, 
+					   /obj/item/gun/ballistic/automatic/pistol/ntusp)
 
+
+/obj/machinery/armaments_dispenser/Initialize()
+	. = ..()
+	inventory = contents.Copy()
+	
 /obj/machinery/armaments_dispenser/ui_assets(mob/user)
 	return list(
 		get_asset_datum(/datum/asset/simple/security_armaments),
@@ -34,14 +43,14 @@
 
 	switch(action)
 		if("dispense_weapon")
-			switch(params["weapon"])
-				if("disabler")
-					new /obj/item/gun/energy/disabler(loc)
-				if("usp") // One NT-USP and a spare magazine
-					new /obj/item/gun/ballistic/automatic/pistol/ntusp(loc)
-					new /obj/item/ammo_box/magazine/recharge/ntusp(loc)
-				else
-					return FALSE
+			if(params["weapon"] && ispath(text2path(params["weapon"])))
+				var/wep = text2path(params["weapon"])
+				new wep(loc)
+				if(params["magazine"] && ispath(text2path(params["magazine"])))
+					var/mag = text2path(params["magazine"])
+					new mag(loc)
+			else
+				return FALSE
 			C.registered_account.sec_weapon_claimed = TRUE
 			return TRUE
 	return FALSE
@@ -56,6 +65,41 @@
 	if (!ui)
 		ui = new(user, src, "ArmamentsDispenser")
 		ui.open()
+
+/obj/machinery/armaments_dispenser/ui_static_data(mob/user)
+	var/list/data = ..()
+	var/list/items = list()
+	for(var/obj/item/wep in inventory)
+		var/obj/item/gun/weapon = wep
+		weapon.update_icon(TRUE)
+		var/icon/gun_icon = getFlatIcon(wep)
+		
+		var/list/details = list()
+		details["name"] = weapon.name
+		details["description"] = weapon.desc
+		details["path"] = weapon.type
+		
+		var/md5 = md5(fcopy_rsc(gun_icon))
+		
+		if(!SSassets.cache["photo_[md5]_icon.png"])
+			SSassets.transport.register_asset("photo_[md5]_icon.png", gun_icon)
+
+		SSassets.transport.send_assets(user, list("photo_[md5]_icon.png" = gun_icon))
+		details["gun_icon"] = SSassets.transport.get_asset_url("photo_[md5]_icon.png")
+		if(istype(weapon, /obj/item/gun/ballistic))
+			var/obj/item/gun/ballistic/gun = wep
+			var/icon/mag_icon = getFlatIcon(gun.magazine)
+			md5 = md5(fcopy_rsc(mag_icon))
+			
+			if(!SSassets.cache["photo_[md5]_icon.png"])
+				SSassets.transport.register_asset("photo_[md5]_icon.png", mag_icon)
+			SSassets.transport.send_assets(user, list("photo_[md5]_icon.png" = mag_icon))
+			details["mag_icon"] = SSassets.transport.get_asset_url("photo_[md5]_icon.png")
+			details["mag_path"] = gun.mag_type
+		items += list(details)
+	
+	data["inventory"] = items
+	return data
 
 /obj/machinery/armaments_dispenser/ui_data(mob/user)
 	var/list/data = ..()
--- tgui/packages/tgui/interfaces/ArmamentsDispenser.js
+++ tgui/packages/tgui/interfaces/ArmamentsDispenser.js
@@ -1,10 +1,11 @@
 import { resolveAsset } from '../assets';
 import { useBackend } from '../backend';
-import { NoticeBox, Button, Flex } from '../components';
+import { NoticeBox, Button, Flex, Box } from '../components';
 import { Window } from '../layouts';
 
 export const ArmamentsDispenser = (props, context) => {
   const { act, data } = useBackend(context);
+  const { inventory = [] } = data;
   return (
     <Window
       title="Armaments Dispenser"
@@ -27,31 +28,51 @@ export const ArmamentsDispenser = (props, context) => {
             You have already claimed a weapon or do not have a registered account!
           </NoticeBox>
         )}
-        <Flex justify="space-between" spacing={1}>
-          <Flex.Item>
-            <Button
-              fluid
-              disabled={!data.allowed || !data.can_claim}
-              width="260px"
-              height="260px"
-              textAlign="center"
-              content={<img src={resolveAsset('disablerbig.png')} />}
-              onClick={() => act('dispense_weapon', {
-                weapon: "disabler",
-              })} />
-          </Flex.Item>
-          <Flex.Item>
-            <Button
-              fluid
-              disabled={!data.allowed || !data.can_claim}
-              width="260px"
-              height="260px"
-              textAlign="center"
-              content={<img src={resolveAsset('ntuspbig.png')} />}
-              onClick={() => act('dispense_weapon', {
-                weapon: "usp",
-              })} />
-          </Flex.Item>
+        <Flex
+          justify="space-between"
+          spacing={1}
+          >
+        {inventory.map(weapon => (
+        <Flex.Item>
+          <Button
+            key={weapon}
+            fluid
+            disabled={!data.allowed || !data.can_claim}
+            width="260px"
+            height="260px"
+            textAlign="center"
+            onClick={() => act('dispense_weapon', {
+              weapon: weapon.path,
+              magazine: weapon.mag_path
+            })} >
+            <Flex
+              height="100%"
+              direction="column">
+              <Box
+                as="img"
+                class="gun_icon"
+                position="relative"
+                src={resolveAsset(weapon.gun_icon)}
+                height="100%"
+                width="100%"
+                style={{
+                  '-ms-interpolation-mode': 'nearest-neighbor'}} />
+              {!!(weapon.mag_icon) && (
+              <Box
+              as="img"
+              class="gun_icon"
+              position="absolute"
+              left="40%"
+              bottom = "10%"
+              src={resolveAsset(weapon.mag_icon)}
+              height="50%"
+              width="50%"
+              style={{
+                '-ms-interpolation-mode': 'nearest-neighbor'}} />)}
+            </Flex>
+          </Button>
+
+      </Flex.Item>))}
         </Flex>
 
       </Window.Content>
