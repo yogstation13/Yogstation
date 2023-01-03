2d455cceaa1ecc7147ca730e23b875b9218be688
Jamie D <993128+JamieD1@users.noreply.github.com>
2023-01-03 17:43:56 +0000

    Commit

--- tgui/packages/tgui-dev-server/dreamseeker.js
+++ tgui/packages/tgui-dev-server/dreamseeker.js
@@ -4,11 +4,12 @@
  * @license MIT
  */
 
-import axios from 'axios';
 import { exec } from 'child_process';
-import { createLogger } from 'common/logging.js';
 import { promisify } from 'util';
+import { createLogger } from './logging.js';
+import { require } from './require.js';
 
+const axios = require('axios');
 const logger = createLogger('dreamseeker');
 
 const instanceByPid = new Map();
@@ -23,10 +24,14 @@ export class DreamSeeker {
   }
 
   topic(params = {}) {
+    // prettier-ignore
     const query = Object.keys(params)
       .map(key => encodeURIComponent(key)
         + '=' + encodeURIComponent(params[key]))
       .join('&');
+    logger.log(
+      `topic call at ${this.client.defaults.baseURL + '/dummy?' + query}`
+    );
     return this.client.get('/dummy?' + query);
   }
 }
@@ -35,7 +40,7 @@ export class DreamSeeker {
  * @param {number[]} pids
  * @returns {DreamSeeker[]}
  */
-DreamSeeker.getInstancesByPids = async pids => {
+DreamSeeker.getInstancesByPids = async (pids) => {
   if (process.platform !== 'win32') {
     return [];
   }
@@ -45,8 +50,7 @@ DreamSeeker.getInstancesByPids = async pids => {
     const instance = instanceByPid.get(pid);
     if (instance) {
       instances.push(instance);
-    }
-    else {
+    } else {
       pidsToResolve.push(pid);
     }
   }
@@ -59,17 +63,21 @@ DreamSeeker.getInstancesByPids = async pids => {
       });
       // Line format:
       // proto addr mask mode pid
-      const entries = stdout
-        .split('\r\n')
-        .filter(line => line.includes('LISTENING'))
-        .map(line => {
-          const words = line.match(/\S+/g);
-          return {
-            addr: words[1],
-            pid: parseInt(words[4], 10),
-          };
-        })
-        .filter(entry => pidsToResolve.includes(entry.pid));
+      const entries = [];
+      const lines = stdout.split('\r\n');
+      for (let line of lines) {
+        const words = line.match(/\S+/g);
+        if (!words || words.length === 0) {
+          continue;
+        }
+        const entry = {
+          addr: words[1],
+          pid: parseInt(words[4], 10),
+        };
+        if (pidsToResolve.includes(entry.pid)) {
+          entries.push(entry);
+        }
+      }
       const len = entries.length;
       logger.log('found', len, plural('instance', len));
       for (let entry of entries) {
@@ -78,13 +86,16 @@ DreamSeeker.getInstancesByPids = async pids => {
         instances.push(instance);
         instanceByPid.set(pid, instance);
       }
-    }
-    catch (err) {
-      logger.error(err);
+    } catch (err) {
+      if (err.code === 'ERR_CHILD_PROCESS_STDIO_MAXBUFFER') {
+        logger.error(err.message, err.code);
+      } else {
+        logger.error(err);
+      }
       return [];
     }
   }
   return instances;
 };
 
-const plural = (word, n) => n !== 1 ? word + 's' : word;
+const plural = (word, n) => (n !== 1 ? word + 's' : word);
