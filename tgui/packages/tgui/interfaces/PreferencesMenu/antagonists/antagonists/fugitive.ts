7d022867d1bdca498edcfd4913fcb4b06f0fac67
Jamie D <993128+JamieD1@users.noreply.github.com>
2023-01-22 13:30:25 +0000

    LORE

--- tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/fugitive.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/fugitive.ts
@@ -1,14 +1,20 @@
 import { Antagonist, Category } from "../base";
 import { multiline } from "common/string";
 
+export const FUGITIVE_MECHANICAL_DESCRIPTION
+   = multiline`Avoid the fugitive hunters sent after you with any allies if you have them, and make sure you stay free by the time the shift ends.
+   `;
+
 const Fugitive: Antagonist = {
   key: "fugitive",
   name: "Fugitive",
   description: [
     multiline`
-    Wherever you come from, you're being hunted. You have 10 minutes to prepare
-    before fugitive hunters arrive and start hunting you and your friends down!
+    Freedom. You gained it somehow, but you're not about to give it up.
+    You're being pursued, and the only friends are those that might be with you.
+    It's unclear who they'll send after you, but a bounty'll be claimed, whether you're dead or alive.
     `,
+    FUGITIVE_MECHANICAL_DESCRIPTION,
   ],
   category: Category.Midround,
 };
