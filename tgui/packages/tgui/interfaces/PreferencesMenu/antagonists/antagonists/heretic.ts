838993fc18a7070b24d908dbf50ac519678d9fd2
Skrem_7 <98909416+Skrem7@users.noreply.github.com>
2023-02-24 16:56:58 -0600

    antag desc update

--- tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/heretic.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/heretic.ts
@@ -3,18 +3,21 @@ import { multiline } from "common/string";
 
 export const HERETIC_MECHANICAL_DESCRIPTION
    = multiline`
-      Find hidden influences and sacrifice crew members to gain magical
-      powers and ascend as one of several paths.
+      Fulfill the requests of your eldritch patrons and become a student of the Mansus via sacrifice and research.
+      Compile ingredients to perform rituals.
+      Seeking ultimate power, complete your studies and ascend, and be counted among the Long.
    `;
+
 
 const Heretic: Antagonist = {
   key: "heretic",
   name: "Heretic",
   description: [
     multiline`
-      Forgotten, devoured, gutted. Humanity has forgotten the eldritch forces
-      of decay, but the mansus veil has weakened. We will make them taste fear
-      again...
+      The dreams began some time back.
+      Stumbling through brambles and trees, you found yourself in a tavern.
+      Unfamiliar faces discuss the Lores that establish the fabric of your reality, and the place with no walls that surrounds all things.
+      They gift you awareness and wish you well.
     `,
     HERETIC_MECHANICAL_DESCRIPTION,
   ],
