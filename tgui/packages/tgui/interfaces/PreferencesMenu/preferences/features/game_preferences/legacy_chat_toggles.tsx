61eb6369cf4ae6088864ff34339bdd1572d06f74
Ling <lingbleed@gmail.com>
2023-02-26 21:30:21 +0200

    Ultra fix

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/legacy_chat_toggles.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/legacy_chat_toggles.tsx
@@ -104,6 +104,5 @@ export const chat_pullr: FeatureToggle = {
 export const chat_typing_indicator: FeatureToggle = {
   name: "Enable typing indicators",
   category: "CHAT",
-  description: "When enabled, you will text bubbles when people are typing.",
   component: CheckboxInput,
 };
