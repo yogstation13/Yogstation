7f7cec2c06ba4007709deaaf14b4b3c6937671ed
adamsong <adamsong@users.noreply.github.com>
2023-05-08 15:46:36 -0400

    Trying this instead

--- tools/travis/check_grep.sh
+++ tools/travis/check_grep.sh
@@ -38,7 +38,7 @@ if grep -i 'centcomm' _maps/**/*.dmm; then
     echo "ERROR: Misspelling(s) of CENTCOM detected in maps, please remove the extra M(s)."
     st=1
 fi;
-if grep '\.proc/' --exclude-from=code/__byond_version_compat.dm code/**/*.dm; then
+if grep '\.proc/' code/**/*.dm | grep -v 'code/__byond_version_compat.dm'; then
 	echo "ERROR: Direct reference to .proc, use PROC_REF instead"
 	st=1
 fi;
