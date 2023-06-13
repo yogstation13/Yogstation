7181dd20de4fa47f3f8f84d1ea6e4ce68d5ee686
JohnFulpWillard <53777086+JohnFulpWillard@users.noreply.github.com>
2023-06-13 16:21:26 -0400

    Update initToast.ps1

--- tools/initToast/initToast.ps1
+++ tools/initToast/initToast.ps1
@@ -2,13 +2,13 @@ param([string]$name, [string]$icon, [Int32]$port=80)
 
 $hkcu = [Microsoft.Win32.RegistryKey]::OpenBaseKey('CurrentUser','default')
 
-$amuid_hkey = $hkcu.CreateSubKey('SOFTWARE\Classes\AppUserModelId\Tgstation.Tgstation', $true, [Microsoft.Win32.RegistryOptions]::Volatile)
+$amuid_hkey = $hkcu.CreateSubKey('SOFTWARE\Classes\AppUserModelId\Yogstation.Yogstation', $true, [Microsoft.Win32.RegistryOptions]::Volatile)
 
 if ($name) { $amuid_hkey.SetValue('DisplayName', $name) }
 if ($icon) { $amuid_hkey.SetValue('IconUri', $icon) }
 
 [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
-$notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('Tgstation.Tgstation')
+$notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('Yogstation.Yogstation')
 
 [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom, ContentType = WindowsRuntime] > $null
 $xml = New-Object Windows.Data.Xml.Dom.XmlDocument
