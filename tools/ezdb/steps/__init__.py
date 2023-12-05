3377995288aacc45293f3ccc5d9c91182d8131be
Ringalong <53777086+JohnFulpWillard@users.noreply.github.com>
2023-12-05 00:01:43 -0500

    updates python stuff to tg

--- tools/ezdb/steps/__init__.py
+++ tools/ezdb/steps/__init__.py
@@ -1,4 +1,4 @@
-rom .download_mariadb import DownloadMariaDB
+from .download_mariadb import DownloadMariaDB
 from .install_database import InstallDatabase
 from .install_initial_schema import InstallInitialSchema
 from .update_schema import UpdateSchema
