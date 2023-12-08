e197527bad08e2ec9b7246171cee858fbca04ea8
Katherine Kiefer <sterlite2902@gmail.com>
2023-12-08 16:50:18 +1100

    arghhh

--- tools/travis/install_extools.sh
+++ tools/travis/install_extools.sh
@@ -4,13 +4,14 @@ set -euo pipefail
 
 echo "Installing Auxmos"
 git clone https://github.com/Putnam3145/auxmos
-cd ../auxmos
+cd ./auxmos
 rustup target add i686-unknown-linux-gnu
 cargo build --target i686-unknown-linux-gnu --features katmos --release
-chmod 755 target/i686-unknown-linux-gnu/release/libauxmos.so
+chmod 755 ./target/i686-unknown-linux-gnu/release/libauxmos.so
 
 
 mkdir -p ~/.byond/bin
-cp target/i686-unknown-linux-gnu/release/libauxmos.so ~/.byond/bin/libauxmos.so
+cp ./target/i686-unknown-linux-gnu/release/libauxmos.so ~/.byond/bin/libauxmos.so
 ldd ~/.byond/bin/libauxmos.so
+cd ..
 echo "Finished installing Auxmos"
