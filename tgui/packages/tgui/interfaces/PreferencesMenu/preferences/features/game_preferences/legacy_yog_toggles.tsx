3ff3639a328aec43620f9f383599916ec7518093
Byemoh <baiomurang@gmail.com>
2024-08-16 13:22:00 -0500

    Update legacy_yog_toggles.tsx

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/legacy_yog_toggles.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/legacy_yog_toggles.tsx
@@ -1,6 +1,7 @@
-import { FeatureToggle, CheckboxInput } from "../base";
+import { FeatureToggle, CheckboxInput, FeatureValueProps } from "../base";
 import { BooleanLike } from "common/react";
 import { useBackend } from "../../../../../backend";
+import { PreferencesMenuData } from "../../../data";
 
 export const quiet_mode: FeatureToggle = {
   name: "Quiet mode",
