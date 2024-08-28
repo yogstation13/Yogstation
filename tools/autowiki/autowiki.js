7e684aa559da288eb4bb8790a455be6f9297e417
wonderinghost <godofh8red@gmail.com>
2024-08-27 23:10:27 -0400

    lets see

--- tools/autowiki/autowiki.js
+++ tools/autowiki/autowiki.js
@@ -36,7 +36,7 @@ async function main() {
 	const bot = new MWBot()
 
 	await bot.loginGetEditToken({
-		apiUrl: "https://tgstation13.org/wiki/api.php",
+		apiUrl: "https://wiki.yogstation.net/wiki/api.php",
 		username: USERNAME,
 		password: PASSWORD,
 	})
