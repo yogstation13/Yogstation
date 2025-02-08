yogstation13/Yogstation

commit 05c51d92b8e43dd6ba0b79726f679ed07175ce27

Minor botany fixes and QoL (#5313)
## About The Pull Request

![2025-02-06
(1738820050)](https://github.com/user-attachments/assets/5e50543d-7615-473b-ae85-9ccce004253b)

![2025-02-06
(1738822508)](https://github.com/user-attachments/assets/86a0d437-67ac-4f9e-8199-92b0628c782f)

![2025-02-06 (1738822515) ~
dreamseeker](https://github.com/user-attachments/assets/5278de3c-3c85-4045-b502-2a020bb97a5a)


## Why It's Good For The Game

makes things less annoying without really affecting balance

## Changelog
:cl:
qol: Made the Biogenerator UI slightly prettier.
fix: The Biogenerator UI now properly calculates the max amount of
something that can be produced.
qol: The composter now actually benefits somewhat from upgraded parts.
qol: The composter now prompts you for how many biocubes you want to
generate, rather than requiring spam-clicking to make multiple.
/:cl:

tgui/packages/tgui/interfaces/Biogenerator.tsx
@@ -1,176 +1,91 @@
+import {
+  Box,
+  Button,
+  Icon,
+  LabeledList,
+  NoticeBox,
+  NumberInput,
+  ProgressBar,
+  Section,
+  Stack,
+  Table,
+  Tabs,
+} from '../components';
 import { BooleanLike } from 'common/react';
 import { classes } from 'common/react';
 import { useBackend, useLocalState } from '../backend';
 import { Window } from '../layouts';
-import {
-  Box,
-  Section,
-  NumberInput,
-  Table,
-  Tabs,
-  LabeledList,
-  NoticeBox,
-  Button,
-  ProgressBar,
-  Stack,
-} from '../components';
 
-type BiogeneratorData = {
-  processing: BooleanLike;
+type Data = {
   beaker: BooleanLike;
-  reagent_color: string;
-  biomass: number;
-  max_visual_biomass: number;
-  can_process: BooleanLike;
   beakerCurrentVolume: number;
   beakerMaxVolume: number;
-  max_output: number;
-  efficiency: number;
+  biomass: number;
+  can_process: BooleanLike;
   categories: Category[];
+  efficiency: number;
+  max_output: number;
+  max_visual_biomass: number;
+  processing: BooleanLike;
+  reagent_color: string;
 };
 
 type Category = {
-  name: string;
   items: Design[];
+  name: string;
 };
 
 type Design = {
-  id: number;
-  name: string;
-  is_reagent: BooleanLike;
-  disable: BooleanLike;
-  cost: number;
   amount: number;
+  cost: number;
+  disable: BooleanLike;
+  id: string;
+  is_reagent: BooleanLike;
+  name: string;
 };
 
-export const Biogenerator = (props) => {
-  const { act, data } = useBackend<BiogeneratorData>();
-  const {
-    processing,
-    beaker,
-    reagent_color,
-    biomass,
-    max_visual_biomass,
-    can_process,
-    beakerCurrentVolume,
-    beakerMaxVolume,
-    max_output,
-    efficiency,
-    categories,
-  } = data;
-  const [selectedCategory, setSelectedCategory] = useLocalState<string>(
+export const Biogenerator = () => {
+  const { data } = useBackend<Data>();
+  const { beaker, beakerCurrentVolume, beakerMaxVolume, categories } = data;
+
+  const [selectedCategory, setSelectedCategory] = useLocalState(
     'category',
     data.categories[0]?.name,
   );
+
   const items =
     categories.find((category) => category.name === selectedCategory)?.items ||
     [];
+
+  const space = beaker ? beakerMaxVolume - beakerCurrentVolume : 1;
+
   return (
-    <Window width={400} height={525}>
+    <Window width={400} height={500}>
       <Window.Content>
         <Stack vertical fill>
           <Stack.Item>
-            <Section fill minHeight="80px">
-              <LabeledList>
-                <LabeledList.Item
-                  label="Biomass"
-                  buttons={
-                    <Button
-                      width={7}
-                      lineHeight={2}
-                      align="center"
-                      icon="cog"
-                      iconSpin={processing ? 1 : 0}
-                      content="Generate"
-                      disabled={!can_process || processing}
-                      onClick={() => act('activate')}
-                    />
-                  }
-                >
-                  <ProgressBar
-                    value={biomass}
-                    minValue={0}
-                    maxValue={max_visual_biomass}
-                    color="good"
-                  >
-                    <Box
-                      lineHeight={1.9}
-                      style={{
-                        'text-shadow': '1px 1px 0 black',
-                      }}
-                    >
-                      {`${parseFloat(biomass.toFixed(2))} units`}
-                    </Box>
-                  </ProgressBar>
-                </LabeledList.Item>
-                {!!beaker && (
-                  <LabeledList.Item
-                    label="Container"
-                    buttons={
-                      <Button
-                        width={7}
-                        lineHeight={2}
-                        align="center"
-                        icon="eject"
-                        content="Eject"
-                        onClick={() => act('eject')}
-                      />
-                    }
-                  >
-                    <ProgressBar
-                      value={beakerCurrentVolume}
-                      minValue={0}
-                      height={2}
-                      maxValue={beakerMaxVolume}
-                      color={reagent_color}
-                    >
-                      <Box
-                        lineHeight={1.9}
-                        style={{
-                          'text-shadow': '1px 1px 0 black',
-                        }}
-                      >
-                        {`${beakerCurrentVolume} of ${beakerMaxVolume} units`}
-                      </Box>
-                    </ProgressBar>
-                  </LabeledList.Item>
-                )}
-                {!beaker && (
-                  <LabeledList.Item label="Container">
-                    <NoticeBox m={0} height={2}>
-                      No liquid container
-                    </NoticeBox>
-                  </LabeledList.Item>
-                )}
-              </LabeledList>
-            </Section>
+            <Controls />
           </Stack.Item>
           <Stack.Item>
             <Tabs fluid>
-              {categories.map((category) => (
+              {categories.map(({ name }) => (
                 <Tabs.Tab
                   align="center"
-                  key={category.name}
-                  selected={category.name === selectedCategory}
-                  onClick={() => setSelectedCategory(category.name)}
+                  key={name}
+                  selected={name === selectedCategory}
+                  onClick={() => setSelectedCategory(name)}
                 >
-                  {category.name}
+                  {name}
                 </Tabs.Tab>
               ))}
             </Tabs>
           </Stack.Item>
-          <Stack.Item grow mt={'2px'}>
+          <Stack.Item grow mt="2px">
             <Section fill scrollable>
               <Table>
-                <ItemList
-                  processing={processing}
-                  biomass={biomass}
-                  items={items}
-                  beaker={beaker}
-                  efficiency={efficiency}
-                  max_output={max_output}
-                  space={beaker ? beakerMaxVolume - beakerCurrentVolume : 1}
-                />
+                {items.map((item) => (
+                  <Item key={item.id} item={item} space={space} />
+                ))}
               </Table>
             </Section>
           </Stack.Item>
@@ -180,65 +95,164 @@ export const Biogenerator = (props) => {
   );
 };
 
-const ItemList = (props) => {
-  const { act } = useBackend();
-  const items = props.items.map((item) => {
-    const [amount, setAmount] = useLocalState(
-      'amount' + item.name,
-      item.is_reagent ? Math.min(Math.max(props.space, 1), 10) : 1,
-    );
-    const disabled =
-      props.processing ||
-      (item.is_reagent && !props.beaker) ||
-      (item.is_reagent && props.space < amount) ||
-      props.biomass < Math.ceil((item.cost * amount) / props.efficiency);
-    const max_possible = Math.floor(
-      (props.efficiency * props.biomass) / item.cost,
-    );
-    const max_capacity = item.is_reagent ? props.space : props.max_output;
-    const max_amount = Math.max(1, Math.min(max_capacity, max_possible));
-    return {
-      ...item,
-      disabled,
-      max_amount,
-      amount,
-      setAmount,
-    };
-  });
-  return items.map((item) => (
-    <Table.Row key={item.id}>
+const Controls = () => {
+  const { act, data } = useBackend<Data>();
+  const {
+    beaker,
+    beakerCurrentVolume,
+    beakerMaxVolume,
+    biomass,
+    can_process,
+    max_visual_biomass,
+    processing,
+    reagent_color,
+  } = data;
+
+  return (
+    <Section fill>
+      <LabeledList>
+        <LabeledList.Item
+          label="Biomass"
+          buttons={
+            <Button
+              width={7}
+              lineHeight={2}
+              align="center"
+              icon="cog"
+              iconSpin={processing ? 1 : 0}
+              disabled={!can_process || processing}
+              onClick={() => act('activate')}
+            >
+              Generate
+            </Button>
+          }
+        >
+          <ProgressBar
+            value={biomass}
+            minValue={0}
+            maxValue={max_visual_biomass}
+            color="good"
+          >
+            <Box
+              lineHeight={1.9}
+              style={{
+                'text-shadow': '1px 1px 0 black',
+              }}
+            >
+              {`${parseFloat(biomass.toFixed(2))} units`}
+            </Box>
+          </ProgressBar>
+        </LabeledList.Item>
+        {!!beaker && (
+          <LabeledList.Item
+            label="Container"
+            buttons={
+              <Button
+                width={7}
+                lineHeight={2}
+                align="center"
+                icon="eject"
+                onClick={() => act('eject')}
+              >
+                Eject
+              </Button>
+            }
+          >
+            <ProgressBar
+              value={beakerCurrentVolume}
+              minValue={0}
+              height={2}
+              maxValue={beakerMaxVolume}
+              color={reagent_color}
+            >
+              <Box
+                lineHeight={1.9}
+                style={{
+                  'text-shadow': '1px 1px 0 black',
+                }}
+              >
+                {`${beakerCurrentVolume} of ${beakerMaxVolume} units`}
+              </Box>
+            </ProgressBar>
+          </LabeledList.Item>
+        )}
+        {!beaker && (
+          <LabeledList.Item label="Container">
+            <NoticeBox m={0} height={2}>
+              No liquid container
+            </NoticeBox>
+          </LabeledList.Item>
+        )}
+      </LabeledList>
+    </Section>
+  );
+};
+
+type Props = {
+  item: Design;
+  space: number;
+};
+
+const Item = (props: Props) => {
+  const { item, space } = props;
+  const { cost, id, is_reagent, name } = item;
+
+  const { act, data } = useBackend<Data>();
+  const { biomass, beaker, efficiency, max_output, processing } = data;
+
+  const minAmount = is_reagent ? Math.min(Math.max(space, 1), 10) : 1;
+
+  const [amount, setAmount] = useLocalState('amount-' + id, minAmount);
+
+  const disabled =
+    processing ||
+    (is_reagent && !beaker) ||
+    (is_reagent && space < amount) ||
+    biomass < Math.ceil(cost * amount);
+
+  const maxPossible = Math.floor(biomass / cost);
+
+  const maxCapacity = is_reagent ? space : max_output;
+  const maxAmount = Math.max(1, Math.min(maxCapacity, maxPossible));
+
+  return (
+    <Table.Row>
       <Table.Cell>
         <span
-          className={classes(['design32x32', item.id])}
+          className={classes(['design32x32', id])}
           style={{
             'vertical-align': 'middle',
           }}
         />{' '}
-        <b>{item.name}</b>
+        <b>{name}</b>
       </Table.Cell>
       <Table.Cell collapsing>
         <NumberInput
-          value={item.amount}
-          width="35px"
+          value={amount}
+          step={1}
+          width="40px"
           minValue={1}
-          maxValue={item.max_amount}
-          onChange={(e, value) => item.setAmount(value)}
+          maxValue={maxAmount}
+          onChange={(_, value) => setAmount(value)}
         />
       </Table.Cell>
       <Table.Cell collapsing>
         <Button
-          fluid
           align="right"
-          content={parseFloat((item.cost * item.amount).toFixed(2)) + ' BIO'}
-          disabled={item.disabled}
+          width={5}
+          pr={0}
+          disabled={disabled}
           onClick={() =>
             act('create', {
-              id: item.id,
-              amount: item.amount,
+              id,
+              amount,
             })
           }
-        />
+        >
+          {parseFloat((cost * amount).toFixed(2)).toLocaleString()}{' '}
+          <Icon name="leaf" />
+        </Button>
       </Table.Cell>
     </Table.Row>
-  ));
+  );
 };

monkestation/code/modules/hydroponics/machines/composter.dm
@@ -10,21 +10,30 @@
 	var/biomatter = 0
 	/// The amount of biomatter needed to make 1 biocube.
 	var/biocube_cost = 40
+	/// Multiplier for how much biomass is created from input.
+	var/input_multiplier = 1
 
 /obj/machinery/composters/Initialize(mapload)
 	. = ..()
 	if(mapload)
-		biomatter = 80
+		biomatter = biocube_cost * 2
+
+/obj/machinery/composters/RefreshParts()
+	. = ..()
+	input_multiplier = src::input_multiplier
+	for(var/datum/stock_part/matter_bin/bin in component_parts)
+		input_multiplier += max((bin.tier - 1) / 10, 0)
+
+	biocube_cost = src::biocube_cost
+	for(var/datum/stock_part/manipulator/manipulator in component_parts)
+		biocube_cost /= 1 + ((manipulator.tier - 1) / 8)
+	biocube_cost = FLOOR(biocube_cost, 5)
 
 /obj/machinery/composters/attacked_by(obj/item/attacking_item, mob/living/user)
 	. = ..()
-	if(istype(attacking_item, /obj/item/seeds))
+	if(istype(attacking_item, /obj/item/seeds) || istype(attacking_item, /obj/item/food))
 		compost(attacking_item)
-
-	if(istype(attacking_item, /obj/item/food))
-		compost(attacking_item)
-
-	if(istype(attacking_item, /obj/item/storage/bag)) // covers any kind of bag that has a compostible item
+	else if(istype(attacking_item, /obj/item/storage/bag)) // covers any kind of bag that has a compostible item
 		var/obj/item/storage/bag/bag = attacking_item
 		var/list/to_compost
 		for(var/item in bag.contents)
@@ -37,7 +46,7 @@
 			compost(to_compost)
 		to_chat(user, span_info("You empty \the [bag] into \the [src]."))
 
-/obj/machinery/gibber/attack_paw(mob/user, list/modifiers)
+/obj/machinery/composters/attack_paw(mob/user, list/modifiers)
 	return attack_hand(user, modifiers)
 
 /obj/machinery/composters/attack_hand(mob/living/user, list/modifiers)
@@ -68,23 +77,39 @@
 			if(C && user.pulling == C && !C.buckled && !C.has_buckled_mobs() && !occupant)
 				user.visible_message(span_danger("[user] stuffs [C] into [src]!"))
 				compost(C, allow_carbons = TRUE)
+		return
 
 	if(biomatter < biocube_cost)
-		to_chat(user, span_notice("Not enough biomatter to produce Bio-Cube"))
+		to_chat(user, span_warning("You need at least [biocube_cost] units of biomatter to produce a biocube."))
 		return
-	new /obj/item/stack/biocube(drop_location(), 1)
-	biomatter -= biocube_cost
-	update_desc()
+
+	var/biocubes_to_produce = tgui_input_number(
+		user,
+		message = "How many biocubes to produce?",
+		title = name,
+		default = 1,
+		max_value = min(floor(biomatter / biocube_cost), /obj/item/stack/biocube::max_amount),
+		min_value = 1,
+	)
+	if(!biocubes_to_produce || QDELETED(user) || QDELETED(src) || !user.can_perform_action(src, FORBID_TELEKINESIS_REACH | ALLOW_RESTING))
+		return
+	var/biomatter_needed = biocubes_to_produce * biocube_cost
+	if(biomatter < biomatter_needed)
+		to_chat(user, span_warning("You need at least [biomatter_needed] units of biomatter to produce [biocubes_to_produce] biocube\s."))
+		return
+	new /obj/item/stack/biocube(drop_location(), biocubes_to_produce)
+	biomatter -= biomatter_needed
 	update_appearance()
 
-/obj/machinery/composters/update_desc()
+/obj/machinery/composters/examine(mob/user)
 	. = ..()
-	desc = "Just insert your bio degradable materials and it will produce compost."
-	desc += "\nBiomatter: [biomatter]"
+	. += "It currently has <b>[biomatter]</b> units of biomatter."
+	. += "It costs <b>[biocube_cost]</b> units of biomatter to produce a biocube."
+	. += "It composts input into biomatter with <b>[input_multiplier]x</b> efficiency."
 
 /obj/machinery/composters/update_overlays()
 	. = ..()
-	if(biomatter < 40)
+	if(biomatter < biocube_cost)
 		. += mutable_appearance('monkestation/icons/obj/machines/composter.dmi', "light_off", layer = OBJ_LAYER + 0.01)
 	else
 		. += mutable_appearance('monkestation/icons/obj/machines/composter.dmi', "light_on", layer = OBJ_LAYER + 0.01)
@@ -120,11 +145,10 @@
 		CHECK_TICK
 	if(!biomatter_added)
 		return
-	biomatter += biomatter_added
+	biomatter += round(biomatter_added * input_multiplier)
 	if(yucky)
 		playsound(loc, 'sound/machines/juicer.ogg', vol = 50, vary = TRUE)
 		audible_message(span_hear("You hear a loud squelchy grinding sound."))
-	update_desc()
 	update_appearance()
 	flick("composter_animate", src)
 

commit be3a4c0bd086e5ee6289f69262d29915d6a92f23

Biogenerator UI followup fixes (#5344)
## About The Pull Request

fixes some minor biogenerator UI issues caused by
https://github.com/Monkestation/Monkestation2.0/pull/5313

## Changelog
:cl:
fix: Fixed the biogenerator UI shifting when adding reagents to a
container.
fix: Made the biogenerator window's default height tall enough to show
everything by default.
/:cl:

tgui/packages/tgui/interfaces/Biogenerator.tsx
@@ -60,7 +60,7 @@ export const Biogenerator = () => {
   const space = beaker ? beakerMaxVolume - beakerCurrentVolume : 1;
 
   return (
-    <Window width={400} height={500}>
+    <Window width={400} height={530}>
       <Window.Content>
         <Stack vertical fill>
           <Stack.Item>
@@ -109,7 +109,7 @@ const Controls = () => {
   } = data;
 
   return (
-    <Section fill>
+    <Section fill minHeight="80px">
       <LabeledList>
         <LabeledList.Item
           label="Biomass"