45faff274f9df50abf18f4ba78f1afc0df031872
Lucy <lucy@absolucy.moe>
2025-03-04 02:46:55 +0000

    tgchat highlight fixes (#5792)
    ## About The Pull Request
    
    ports the following tg prs:
    - https://github.com/tgstation/tgstation/pull/77228
    - https://github.com/tgstation/tgstation/pull/89723
    
    ## Why It's Good For The Game
    
    things working properly is good
    
    ## Changelog
    :cl:
    fix: (GoldenAlpharex) Chat highlights now escape special RegEx
    characters from non-RegEx highlights.
    fix: (GoldenAlpharex) Broken RegEx expressions no longer cause the chat
    to bluescreen, allowing you to properly fix them.
    fix: Fixed more advanced regex-based chat highlights not working.
    /:cl:
    
    ---------
    
    Co-authored-by: GoldenAlpharex <58045821+GoldenAlpharex@users.noreply.github.com>

--- tgui/packages/tgui-panel/chat/renderer.jsx
+++ tgui/packages/tgui-panel/chat/renderer.jsx
@@ -205,6 +205,7 @@ class ChatRenderer {
       const matchCase = setting.matchCase;
       const enabled = setting.enabled;
       const allowedRegex = /^[a-zа-яё0-9_\-$/^[\s\]\\]+$/gi;
+      const regexEscapeCharacters = /[!#$%^&*)(+=.<>{}[\]:;'"|~`_\-\\/]/g;
       const lines = String(text)
         .split(',')
         .map((str) => str.trim())
@@ -214,7 +215,8 @@ class ChatRenderer {
             str &&
             str.length > 1 &&
             // Must be alphanumeric (with some punctuation)
-            allowedRegex.test(str) &&
+            (allowedRegex.test(str) ||
+              (str.charAt(0) === '/' && str.charAt(str.length - 1) === '/')) &&
             // Reset lastIndex so it does not mess up the next word
             ((allowedRegex.lastIndex = 0) || true),
         );
@@ -240,19 +242,29 @@ class ChatRenderer {
           if (!highlightWords) {
             highlightWords = [];
           }
+          // We're not going to let regex characters fuck up our RegEx operation.
+          line = line.replace(regexEscapeCharacters, '\\$&');
+
           highlightWords.push(line);
         }
       }
       const regexStr = regexExpressions.join('|');
       const flags = 'g' + (matchCase ? '' : 'i');
-      // setting regex overrides matchword
-      if (regexStr) {
-        highlightRegex = new RegExp('(' + regexStr + ')', flags);
-      } else {
-        const pattern = `${matchWord ? '\\b' : ''}(${lines.join('|')})${
-          matchWord ? '\\b' : ''
-        }`;
-        highlightRegex = new RegExp(pattern, flags);
+      // We wrap this in a try-catch to ensure that broken regex doesn't break
+      // the entire chat.
+      try {
+        // setting regex overrides matchword
+        if (regexStr) {
+          highlightRegex = new RegExp('(' + regexStr + ')', flags);
+        } else {
+          const pattern = `${matchWord ? '\\b' : ''}(${highlightWords.join(
+            '|',
+          )})${matchWord ? '\\b' : ''}`;
+          highlightRegex = new RegExp(pattern, flags);
+        }
+      } catch {
+        // We just reset it if it's invalid.
+        highlightRegex = null;
       }
       // Lazy init
       if (!this.highlightParsers) {
